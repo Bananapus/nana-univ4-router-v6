1cfa263774afb7959859784ea6bddd66c20e70b4
Jango <me.jango@protonmail.com>
2026-05-22 15:09:15 -0300

    chore: bump @bananapus/core-v6 to ^0.0.59 + patch missed cashOutTokensOf call sites

--- test/regression/BuybackCashOutMetadataIgnored.t.sol
+++ test/regression/BuybackCashOutMetadataIgnored.t.sol
@@ -229,7 +229,7 @@ contract BuybackCashOutMetadataIgnoredTest is JuiceboxHookTest {
         _installMetadataOnlySellTerminal(2 ether);
 
         uint256 reclaimed = metadataOnlySellTerminal.cashOutTokensOf(
-            address(this), 123, 1 ether, address(token1), 1.5 ether, payable(address(this)), bytes("")
+            address(this), 123, 1 ether, address(token1), 1.5 ether, payable(address(this)), bytes(""), 0
         );
 
         assertEq(reclaimed, 2 ether, "terminal itself can satisfy the minimum");
